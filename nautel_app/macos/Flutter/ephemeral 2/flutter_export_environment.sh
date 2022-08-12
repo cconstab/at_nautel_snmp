186a8fe924b439af6f350cc778197bde8a5d61da
Colin Constable <colin@constable.cc>
2022-08-11 18:43:08 -0700

    mac

---
+++ nautel_app/macos/Flutter/ephemeral 2/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/cconstab/flutter-arm64"
+export "FLUTTER_APPLICATION_PATH=/Users/cconstab/Documents/GitHub/cconstab/at_nautel_snmp/nautel_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/cconstab/Documents/GitHub/cconstab/at_nautel_snmp/nautel_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/cconstab/Documents/GitHub/cconstab/at_nautel_snmp/nautel_app/.dart_tool/package_config.json"
