39174d092cbe1046f84843630fa039a0b5a4c3e0
Colin Constable <colin@constable.cc>
2023-02-26 11:47:03 -0800

    feat: Updated android manifest for prod release

--- nautel_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ nautel_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import at_file_saver
 import biometric_storage
 import file_selector_macos
 import package_info_plus_macos
-import path_provider_macos
+import path_provider_foundation
 import screen_retriever
 import share_plus_macos
 import url_launcher_macos
